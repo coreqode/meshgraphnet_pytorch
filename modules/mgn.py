29e8fe871e22342c33977965a19176e3c9b275c3
coreqode <chinu.deep100@gmail.com>
2022-04-16 11:21:17 +0530

    training

--- modules/mgn.py
+++ modules/mgn.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from base.base_module import BaseModule
 from nets.cloth_model import Model
 from datasets.dataset import FlagSimpleDataset
+from utils import common
 
 class MGN(BaseModule):
     def __init__(self):
@@ -38,22 +39,22 @@ class MGN(BaseModule):
     def define_model(self):
         self.model = Model(self.device, size =3)
 
-    def loss_function(self, model_inputs, data):
+    def loss_func(self, data, predictions):
         world_pos = data['world_pos']
         prev_world_pos = data['prev|world_pos']
         target_world_pos = data['target|world_pos']
-
         cur_position = world_pos
         prev_position = prev_world_pos
         target_position = target_world_pos
         target_acceleration = target_position - 2 * cur_position + prev_position
-        target_normalized = model.get_output_normalizer()(target_acceleration).to(device)
+        target_normalized = self.model.get_output_normalizer()(target_acceleration)
 
-        # build loss
-        node_type = data['node_type']
-        loss_mask = torch.eq(node_type[:, 0], torch.tensor([common.NodeType.NORMAL.value], device=device).int())
-        error = torch.sum((target_normalized - network_output) ** 2, dim=1)
-        loss = torch.mean()
+        node_type = data['node_type'][0]
+        loss_mask = torch.eq(node_type[:, 0], torch.tensor([common.NodeType.NORMAL.value]).to(self.device).int())
+        error = (target_normalized[0] - predictions) ** 2
+        error = torch.sum(error , dim = 1)
+        error = torch.mean(error[loss_mask])
+        loss = {'mse_loss': error}
         return loss
 
     def inspect_dataset(self):
